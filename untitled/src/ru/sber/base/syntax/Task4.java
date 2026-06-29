fe648b7aeae55437325de02df034ba81f1a0cf75
orlanorlanovich <orlan.orlanovich@gmail.com>
2026-06-29 16:16:13 +0300

    Выполнено 4 задание практики

---
+++ untitled/src/ru/sber/base/syntax/Task4.java
@@ -0,0 +1,22 @@
+package ru.sber.base.syntax;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите трёхзначное число: ");
+        int n = scanner.nextInt();
+
+        int hundreds = n / 100;
+        int tens = (n / 10) % 10;
+        int ones = n % 10;
+
+        int sum = hundreds + tens + ones;
+
+        System.out.println("Сумма цифр числа " + n + " = " + sum);
+
+        scanner.close();
+    }
+}

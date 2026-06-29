74089055a747822a2130f3078a58f5854a677445
orlanorlanovich <orlan.orlanovich@gmail.com>
2026-06-29 16:23:46 +0300

    Выполнено 6 задание практики

---
+++ untitled/src/ru/sber/base/syntax/Task6.java
@@ -0,0 +1,37 @@
+package ru.sber.base.syntax;
+
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите натуральное число: ");
+        int n = scanner.nextInt();
+
+        // Проверка на минимальные значения
+        if (n < 2) {
+            System.out.println(n + " не является простым числом");
+            scanner.close();
+            return;
+        }
+
+        boolean isPrime = true;
+
+        // Проверяем делители от 2 до корня из n
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;  // Выходим из цикла, так как уже нашли делитель
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " является простым числом");
+        } else {
+            System.out.println(n + " не является простым числом");
+        }
+
+        scanner.close();
+    }
+}

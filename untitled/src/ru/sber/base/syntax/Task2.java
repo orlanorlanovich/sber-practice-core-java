1d95490aabfde1bb44fbe1976034a83313257916
orlanorlanovich <orlan.orlanovich@gmail.com>
2026-06-29 16:05:06 +0300

    Выполнено 2 задание практики

---
+++ untitled/src/ru/sber/base/syntax/Task2.java
@@ -0,0 +1,13 @@
+package ru.sber.base.syntax;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 21;
+        int b = 8;
+
+        int cel = a / b;
+        int ost = a % b;
+
+        System.out.println(a + " / " + b + " = " + cel + " и " + ost + " в остатке");
+    }
+}
